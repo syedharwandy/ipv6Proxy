4a4302b0cb6add8cef3b7b22d02e151e986c6262
syedharwandy <syedharwandy2@gmail.com>
2024-05-19 23:10:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import express from 'express'
 import shell from 'shelljs'
 const app = express()
 
-const availableHttpPort = ['1111', '2222', '3333', '4444', '5555', '6666']
-const availableSocksPort = ['1111', '2222', '3333', '4444', '5555', '6666']
+const availableHttpPort = ['1000', '1001', '1002', '1003', '1004', '1005', '1006', '1007', '1008', '1009', '1010']
+const availableSocksPort = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010']
 const username = 'syedharwandy'
 const password = 'Asyraf1994'
 
@@ -13,9 +13,15 @@ app.get('/', (req, res) => {
 	res.json({ Ip: '192.168.0.1', Port: '1223', Username: 'syedafs', Password: '123213213' })
 })
 
+app.get('/restartProxy', (req, res) => {
+	shell.exec(`systemctl stop 3proxy.service`)
+	shell.exec(`systemctl start 3proxy.service`)
+	res.send('Proxy Restart')
+})
+
 //Set Ipv6 Proxy
 app.post('/setipv6proxy', async (req, res) => {
-	const ipv6Proxy = await req.body['Ipv6 Proxy']
+	const ipv6Address = await req.body['Ipv6 Address']
 	const localIp = await req.body['Local IP']
 
 	//Get Random Available Http Port
@@ -30,24 +36,34 @@ app.post('/setipv6proxy', async (req, res) => {
 
 	// console.log(currentPort)
 	shell
-		.ShellString(`proxy -p${currentHttpPort} -a -n -6 -i0.0.0.0 -e${await ipv6Proxy}\n`)
-		.toEnd('/usr/local/etc/3proxy/cfg/3proxy.cfg') //Http
+		.ShellString(`proxy -p${currentHttpPort} -a -n -6 -i0.0.0.0 -e${await ipv6Address}\n`)
+		.toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Http
 	shell
-		.ShellString(`socks -p${currentSockPort} -a -n -6 -i0.0.0.0 -e${await ipv6Proxy}\n`)
-		.toEnd('/usr/local/etc/3proxy/cfg/3proxy.cfg') //Socks
-	// shell.exec(`ip -6 addr add ${await ipv6Proxy}/64 dev enp0s3`)
+		.ShellString(`socks -p${currentSockPort} -a -n -6 -i0.0.0.0 -e${await ipv6Address}\n`)
+		.toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Socks
+	shell.exec(`ip -6 addr add ${await ipv6Address}/64 dev enp0s3`)
 
 	res.json({
 		LocalIp: localIp,
 		HttpPort: currentHttpPort,
 		SocksPort: currentSockPort,
-		Ipv6Address: await ipv6Proxy,
+		Ipv6Address: await ipv6Address,
 		Username: username,
 		Password: password,
 	})
 })
 //Remove Ipv6 Proxy
-app.post('/removeipv6proxy', async (req, res) => {})
+app.post('/removeipv6proxy', async (req, res) => {
+	const httpPort = req.body['HttpPort']
+	const socksPort = req.body['SocksPort']
+	const ipv6Address = req.body['Ipv6Address']
+
+	shell.sed('-i', `proxy -p${httpPort} -a -n -6 -i0.0.0.0 -e${await ipv6Address}`, '', '/usr/local/3proxy/conf/3proxy.cfg')
+	shell.sed('-i', `socks -p${socksPort} -a -n -6 -i0.0.0.0 -e${await ipv6Address}`, '', '/usr/local/3proxy/conf/3proxy.cfg')
+
+	shell.exec(`ip -6 addr del ${await ipv6Address}/64 dev enp0s3`)
+	res.send('Ipv6 Address Remove')
+})
 
 app.listen(4000, () => {
 	console.log('Listen To 4000')
