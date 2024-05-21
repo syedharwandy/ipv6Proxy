7b1f7374a7b099bb65fbf52f8a5b98ad8ad2fc77
syedharwandy <syedharwandy2@gmail.com>
2024-05-21 19:57:34 +0800

    New

--- src/main.js
+++ src/main.js
@@ -1,89 +1,111 @@
 import express from 'express'
 import shell from 'shelljs'
 import Queue from './dataQueue.js'
+import asyncHandler from 'express-async-handler'
+const AsyncLock = require('async-lock')
 
+const lock = new AsyncLock()
 const httpQue = new Queue()
 const app = express()
 const serverPort = 4000
-
-let availableHttpPort = [
-	10000, 10001, 10002, 10003, 10004, 10005, 10006, 10007, 10008, 10009, 10010, 10011, 10012, 10013, 10014, 10015, 10016, 10017,
-	10018, 10019, 10020, 10021, 10022, 10023, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 10033, 10034, 10035,
-	10036, 10037, 10038, 10039, 10040, 10041, 10042, 10043, 10044, 10045, 10046, 10047, 10048, 10049, 10050,
-]
-
-for (let x = 0; x < availableHttpPort.length; x++) {
-	httpQue.enqueue(availableHttpPort[x])
-}
-
-function autoRemoveConnection(httpPort, socksPort, ipv6Address) {
-	setTimeout(() => {
-		shell.echo(`Remove Unused Port [H:${httpPort}|S:${socksPort}] For [${ipv6Address}]`)
-		//Remove Port From File
-		const newRegexHttp = new RegExp(`.*-p${httpPort}.*`, 'd') //Working
-		const newRegexSocks = new RegExp(`.*-p${socksPort}.*`, 'd') //Working
-		shell.sed('-i', newRegexHttp, '', '/usr/local/3proxy/conf/3proxy.cfg')
-		shell.sed('-i', newRegexSocks, '', '/usr/local/3proxy/conf/3proxy.cfg')
-
-		shell.exec(`ip -6 addr del ${ipv6Address}/64 dev enp0s3`)
-	}, 10 * 60000)
-}
-
+const totalPortNeedToBuffer = 100
 const username = 'syedharwandy'
 const password = 'Asyraf1994'
 
-app.use(express.json())
-
-app.get('/startProxy', (req, res) => {
-	shell.echo(`Start IPV6 PROXY Using Port ${serverPort}`)
-	shell
-		.ShellString(
-			'nscache 65536\nnserver 8.8.8.8\nnserver 8.8.4.4\n\nconfig /conf/3proxy.cfg\nmonitor /conf/3proxy.cfg\n\ncounter /count/3proxy.3cf\nusers $/conf/passwd\n\ninclude /conf/counters\ninclude /conf/bandlimiters\n\nauth strong\nallow *\n'
-		)
-		.to('/usr/local/3proxy/conf/3proxy.cfg') //New Cfg File
-
-	setTimeout((r) => r, 20000)
-	shell.chmod(700, '/usr/local/3proxy/conf/3proxy.cfg')
-	shell.exec(`systemctl stop 3proxy.service`)
-	setTimeout((r) => r, 20000)
-	shell.exec(`systemctl start 3proxy.service`)
-
-	res.send('Proxy Restart')
-})
-//Set Ipv6 Proxy
-app.post('/setipv6proxy', (req, res) => {
-	let ipv6Address = req.body['Ipv6 Address']
-	const localIp = req.body['Local IP']
-
-	//Get Random Available Http Port
-	const httpPort = httpQue.dequeue()
-	const socksPort = httpPort + 10000
-	httpQue.enqueue(httpPort)
-	shell.echo(`Setup New Ipv6 Using Port [H:${httpPort}|S:${socksPort}] For [${ipv6Address}]`)
-
-	//Remove Used Port
-	const newRegexHttp = new RegExp(`.*-p${httpPort}.*`, 'd') //Working
-	const newRegexSocks = new RegExp(`.*-p${socksPort}.*`, 'd') //Working
-	shell.sed('-i', newRegexHttp, '', '/usr/local/3proxy/conf/3proxy.cfg')
-	shell.sed('-i', newRegexSocks, '', '/usr/local/3proxy/conf/3proxy.cfg')
-
-	// console.log(currentPort)
-	shell.exec(`ip -6 addr add ${ipv6Address}/64 dev enp0s3`)
-	shell.ShellString(`proxy -p${httpPort} -a -n -6 -i0.0.0.0 -e${ipv6Address}\n`).toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Http
-	shell.ShellString(`socks -p${socksPort} -a -n -6 -i0.0.0.0 -e${ipv6Address}\n`).toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Socks
-
-	shell.exec(`ufw allow ${httpPort}`)
-	shell.exec(`ufw allow ${socksPort}`)
-
-	autoRemoveConnection(httpPort, socksPort, ipv6Address)
-
-	res.json({
-		LocalIp: localIp,
-		HttpPort: httpPort,
-		SocksPort: socksPort,
-		Ipv6Address: ipv6Address,
-		Username: username,
-		Password: password,
+//shelljs Exec Promises
+async function runShellexec(command) {
+	return new Promise((resolve, reject) => {
+		shell.exec(command, { async: true }, (code, stdout, stderr) => {
+			if (!code) {
+				return resolve(stdout)
+			}
+			return reject(stderr)
+		})
 	})
-})
+}
+//Function To Remove Ipv6
+function autoRemoveConnection(httpPort, socksPort, ipv6Address) {
+	setTimeout(async () => {
+		await lock.acquire('removeUsedIPV6', async () => {
+			shell.echo(`Remove Unused Port [H:${httpPort}|S:${socksPort}] For [${ipv6Address}]`)
+			//Remove Port From File
+			const newRegexHttp = new RegExp(`.*-p${httpPort}.*`, 'd') //Working
+			const newRegexSocks = new RegExp(`.*-p${socksPort}.*`, 'd') //Working
+			shell.sed('-i', newRegexHttp, '', '/usr/local/3proxy/conf/3proxy.cfg')
+			shell.sed('-i', newRegexSocks, '', '/usr/local/3proxy/conf/3proxy.cfg')
+
+			await runShellexec(`ip -6 addr del ${ipv6Address}/64 dev enp0s3`)
+		})
+	}, 10 * 60000)
+}
+//Create HttpPort
+for (let x = 1; x <= totalPortNeedToBuffer; x++) {
+	httpQue.enqueue(availableHttpPort[10000 + x])
+}
+//Used Express JSON
+app.use(express.json())
+//Start IPV6 Proxy
+app.get(
+	'/startProxy',
+	asyncHandler(async (req, res) => {
+		shell.echo(`Start IPV6 PROXY Using Port ${serverPort}`)
+		shell
+			.ShellString(
+				'nscache 65536\nnserver 8.8.8.8\nnserver 8.8.4.4\n\nconfig /conf/3proxy.cfg\nmonitor /conf/3proxy.cfg\n\ncounter /count/3proxy.3cf\nusers $/conf/passwd\n\ninclude /conf/counters\ninclude /conf/bandlimiters\n\nauth strong\nallow *\n'
+			)
+			.to('/usr/local/3proxy/conf/3proxy.cfg') //New Cfg File
+
+		await new Promise((r) => setTimeout(r, 20000))
+		shell.chmod(700, '/usr/local/3proxy/conf/3proxy.cfg')
+		await runShellexec(`systemctl stop 3proxy.service`)
+		await new Promise((r) => setTimeout(r, 20000))
+		await runShellexec(`systemctl start 3proxy.service`)
+
+		res.send('Proxy Restart')
+	})
+)
+//Set Ipv6 Proxy
+app.get(
+	'/setipv6proxy',
+	asyncHandler(async (req, res) => {
+		await lock.acquire('createIPv6', async () => {
+			let ipv6Address = req.body['Ipv6 Address']
+			const localIp = req.body['Local IP']
+
+			//Get Random Available Http Port
+			const httpPort = httpQue.dequeue()
+			const socksPort = httpPort + 10000
+			httpQue.enqueue(httpPort)
+			shell.echo(`Setup New Ipv6 Using Port [H:${httpPort}|S:${socksPort}] For [${ipv6Address}]`)
+
+			//Remove Used Port
+			const newRegexHttp = new RegExp(`.*-p${httpPort}.*`, 'd') //Working
+			const newRegexSocks = new RegExp(`.*-p${socksPort}.*`, 'd') //Working
+			shell.sed('-i', newRegexHttp, '', '/usr/local/3proxy/conf/3proxy.cfg')
+			shell.sed('-i', newRegexSocks, '', '/usr/local/3proxy/conf/3proxy.cfg')
+
+			// console.log(currentPort)
+			await runShellexec(`ip -6 addr add ${ipv6Address}/64 dev enp0s3`)
+			shell.ShellString(`proxy -p${httpPort} -a -n -6 -i0.0.0.0 -e${ipv6Address}\n`).toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Http
+			shell
+				.ShellString(`socks -p${socksPort} -a -n -6 -i0.0.0.0 -e${ipv6Address}\n`)
+				.toEnd('/usr/local/3proxy/conf/3proxy.cfg') //Socks
+
+			await runShellexec(`ufw allow ${httpPort}`)
+			await runShellexec(`ufw allow ${socksPort}`)
+
+			autoRemoveConnection(httpPort, socksPort, ipv6Address)
+
+			res.json({
+				LocalIp: localIp,
+				HttpPort: httpPort,
+				SocksPort: socksPort,
+				Ipv6Address: ipv6Address,
+				Username: username,
+				Password: password,
+			})
+		})
+	})
+)
+
 app.listen(serverPort)
